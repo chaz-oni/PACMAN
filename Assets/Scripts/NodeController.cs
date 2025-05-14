851b86d0dd7e3bad2858d6b32aae0e70f066e4e5
José Isai Galvan Villegas <160529629+Isai2222@users.noreply.github.com>
2025-05-13 23:32:56 -0600

    scriptnodecontroller
    Script para verificar movimientos

---
+++ Assets/Scripts/NodeController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class NodeController : MonoBehaviour
+{
+    public bool canMoveLeft = false;
+    public bool canMoveRigth = false;
+    public bool canMoveUp = false;
+    public bool canMoveDown = false;
+    public GameObject nodeLeft;
+    public GameObject nodeRight;
+    public GameObject nodeUp;
+    public GameObject nodeDown;
+
+    void Start()
+    {
+        RaycastHit2D[] hitsDown;
+        //Raycast hacia abajo
+        hitsDown = Physics2D.RaycastAll(transform.position, -Vector2.up);
+        for (int i = 0; i < hitsDown.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsDown[i].point.y - transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveDown = true;
+                nodeDown = hitsDown[i].collider.gameObject;
+            }
+        }
+        RaycastHit2D[] hitsUp;
+        //Raycast hacia abajo
+        hitsUp = Physics2D.RaycastAll(transform.position, -Vector2.up);
+        for (int i = 0; i < hitsUp.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsUp[i].point.y - transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveUp = true;
+                nodeUp = hitsUp[i].collider.gameObject;
+            }
+        }
+        RaycastHit2D[] hitsRight;
+        //Raycast hacia abajo
+        hitsRight = Physics2D.RaycastAll(transform.position, -Vector2.up);
+        for (int i = 0; i < hitsRight.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsRight[i].point.x - transform.position.x);
+            if (distance < 0.4f)
+            {
+                canMoveRigth = true;
+                nodeRight = hitsRight[i].collider.gameObject;
+            }
+        }
+        RaycastHit2D[] hitsLeft;
+        //Raycast hacia abajo
+        hitsLeft = Physics2D.RaycastAll(transform.position, -Vector2.up);
+        for (int i = 0; i < hitsLeft.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsLeft[i].point.x - transform.position.x);
+            if (distance < 0.4f)
+            {
+                canMoveLeft = true;
+                nodeLeft = hitsLeft[i].collider.gameObject;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+}
