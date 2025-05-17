a136d63629f2f0c0d2279874298e88b08957ba0c
José Isai Galvan Villegas <160529629+Isai2222@users.noreply.github.com>
2025-05-16 19:33:34 -0600

    dots
    Dots creados

--- Assets/Scripts/SpawnNodes.cs
+++ Assets/Scripts/SpawnNodes.cs
@@ -3,20 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 public class SpawnNodes : MonoBehaviour
 {
-    int numToSpawn = 25;
-    public float spawnOffSet = 0.3f;
+    int numToSpawn = 26;
     public float currentSpawnOffset;
+
+    public float spawnOffSet = 0.3f;
+
     void Start()
     {
-        if (gameObject.name == "Node")
-        {
-            for (int i = 0; i < numToSpawn; i++)
-            {
-                //Duplicar el nodo
-                GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x + currentSpawnOffset, transform.position.y, 0), Quaternion.identity);
-                currentSpawnOffset += spawnOffSet;
-            }
-        }
+        // gameObject.name = "Node";
+        // currentSpawnOffset = 0;
+        // return;
+
+        // if (gameObject.name == "Node")
+        // {
+        //     for (int i = 0; i < numToSpawn; i++)
+        //     {
+        //         //Duplicar el nodo
+        //         GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x + currentSpawnOffset, transform.position.y, 0), Quaternion.identity);
+        //         currentSpawnOffset += spawnOffSet;
+        //     }
+        // }
 
     }
 
